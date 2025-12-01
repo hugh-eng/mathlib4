ded92fa3c79e2ff8a5f93a089ffa307eee3df1b3
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-12-01 23:10:49 +0100

    chore: tidy various files

--- Mathlib/RingTheory/TensorProduct/IsBaseChangeHom.lean
+++ Mathlib/RingTheory/TensorProduct/IsBaseChangeHom.lean
@@ -36,10 +36,10 @@ open LinearMap TensorProduct
 
 variable {R : Type*} [CommSemiring R]
     (S : Type*) [CommSemiring S] [Algebra R S]
-    (M : Type*) [AddCommMonoid M] [Module R M] -- [Module S M] [IsScalarTower R S M]
+    (M : Type*) [AddCommMonoid M] [Module R M]
     {N : Type*} [AddCommMonoid N] [Module R N]
     {ι : Type*} [DecidableEq ι]
-    {P : Type*} [AddCommMonoid P] [Module R P] -- [Module S P] [IsScalarTower R S P]
+    {P : Type*} [AddCommMonoid P] [Module R P]
 
 section LinearMapRight
 
